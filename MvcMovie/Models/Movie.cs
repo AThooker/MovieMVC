27e46459a9c77bc48030362ef4839fa02b96865f
Austin Hooker <ahooker@elevenfifty.org>
2021-10-21 16:14:37 -0400

    firstCommitMovieApp

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        //You can put constraints on same line
+        [StringLength(60, MinimumLength = 3), Required]
+        public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
+        public string Genre { get; set; }
+        //a max range of 18 decimals with 2 of those being after the decimal. All records will have two numbers after decimal
+        [Range(1,1000)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; }
+    }
+}
---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MvcMovieContext>>()))
+            {
+                //Look for any movies
+                if (context.Movie.Any())
+                {
+                    return; //Db is already seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Pulp Fiction",
+                        ReleaseDate = DateTime.Parse("1994-10-14"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "PG"
+                    },
+                    new Movie
+                    {
+                        Title = "Step Brothers",
+                        ReleaseDate = DateTime.Parse("2008-07-28"),
+                        Genre = "Comedy",
+                        Price = 15.99M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Law Abiding Citizen",
+                        ReleaseDate = DateTime.Parse("2009-10-16"),
+                        Genre = "Action Thriller",
+                        Price = 20.99M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Up",
+                        ReleaseDate = DateTime.Parse("2009-5-29"),
+                        Genre = "Animation Adventure",
+                        Price = 7.99M,
+                        Rating = "PG"
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
